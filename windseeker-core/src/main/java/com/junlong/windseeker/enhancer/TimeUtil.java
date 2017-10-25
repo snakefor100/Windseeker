3e4c15774084efff7fa133eb4ad530909a804f4d
牛俊龙 <540973185@qq.com>
2017-10-26 00:23:42 +0800

    init

--- windseeker-core/src/main/java/com/junlong/windseeker/enhancer/TimeUtil.java
+++ windseeker-core/src/main/java/com/junlong/windseeker/enhancer/TimeUtil.java
@@ -1,5 +1,9 @@
 package com.junlong.windseeker.enhancer;
 
+import com.junlong.windseeker.command.asm.AdviceWeaver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +11,7 @@ import java.util.Map;
  * Created by niujunlong on 17/10/16.
  */
 public class TimeUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(AdviceWeaver.class);
     private static Map<String, Long> startTimes = new HashMap<String, Long>();
     private static Map<String, Long> endTimes   = new HashMap<String, Long>();
 
@@ -14,10 +19,13 @@ public class TimeUtil {
     }
 
     public static long getStartTime(String key) {
+
         return startTimes.get(key);
     }
 
     public static void setStartTime(String key) {
+        LOG.info("设置开始时间: {}",key);
+
         startTimes.put(key, System.currentTimeMillis());
     }
 
@@ -26,13 +34,20 @@ public class TimeUtil {
     }
 
     public static void setEndTime(String key) {
+        LOG.info("设置结束时间: {}",key);
         endTimes.put(key, System.currentTimeMillis());
     }
 
-    public static long getExclusiveTime(String className, String methodName, String methodDesc) {
-        String key = className + methodName + methodDesc;
+    public static long getExclusiveTime(String key) {
+
         long exclusive = getEndTime(key) - getStartTime(key);
-        System.out.println(className.replace("/", ".") + "." + methodName + " exclusive:" + exclusive);
+        LOG.info("总时间: {}",exclusive);
         return exclusive;
+    }
+
+    public static void main(String[] args) {
+        TimeUtil.setStartTime("11");
+        TimeUtil.setEndTime("11");
+        TimeUtil.getExclusiveTime("11");
     }
 }
